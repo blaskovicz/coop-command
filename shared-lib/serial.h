519b31b06c01a3a562061a35cb787b1e2c3f6e61
Zach Auclair <zach101@gmail.com>
2020-07-03 12:08:13 -0400

    refactor(shared-lib/serial): use macros for debug print wrappers

--- shared-lib/serial.h
+++ shared-lib/serial.h
@@ -1,15 +1,31 @@
 #ifndef SERIAL_SHARED_H
 #define SERIAL_SHARED_H
+
+#ifdef DEBUG_MODE
+
+#define _PRINT(...) Serial.print(__VA_ARGS__)
+#define _PRINTLN(...) Serial.println(__VA_ARGS__)
+#define _FLUSH() Serial.flush()
+
+#else
+
+#define _PRINT(...)
+#define _PRINTLN(...)
+#define _FLUSH(...)
+
+#endif
+
 void serialInit()
 {
     // set up serial monitor and wait for it to open
-    Serial.begin(9600);
+    Serial.begin(115200);
 
     do
     {
         delay(100);
     } while (!Serial);
 
-    Serial.println("[serial] initialized");
+    _PRINTLN("[serial] initialized");
 }
+
 #endif
